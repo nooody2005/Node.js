dadb027fde9a7cc57c981777ef2313298949b6f4
Nada Hashem <Nooody2005std@gmail.com>
2026-03-09 12:29:12 +0200

    advanced api done

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -4,8 +4,22 @@ const Tour = require('./../models/tourModel');
 //get all tours
 exports.getAllTours = async (req, res) => {
     try{
+        console.log(req.query);
+        //BUILD QUERY
+        //1)FILTERING
+        const queryObj = {...req.query};
+        const excludedFields = ['page','sort','limit','fields'];
+        excludedFields.forEach(el => delete queryObj[el]);
 
-        const tours = await Tour.find();
+        //2)ADVANCED FILTERING
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g ,match => `$${match}`);
+        console.log(JSON.parse(queryStr));
+
+        const tours = await Tour.find(JSON.parse(queryStr));
+
+        // const tours = await Tour.find(queryObj);
+        // const tours = await Tour.find();
         
         res.status(200).json({
         status: 'success',
