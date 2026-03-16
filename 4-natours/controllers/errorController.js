64a7a8d16b892a8aea4fa72f297a61cb5305ef13
Nada Hashem <Nooody2005std@gmail.com>
2026-03-16 07:22:10 +0200

    Handling Invalid DB id

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,3 +1,8 @@
+const AppError = require('./../utils/appError');
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}`; 
+    return new AppError(message,400);
+}
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -39,7 +44,12 @@ module.exports = (err, req, res, next) => {
   if(process.env.NODE_ENV === 'development'){
     sendErrorDev(err,res);
   }else if (process.env.NODE_ENV === 'production'){
-    sendErrorProd(err,res);
+    //let error = { ...err };
+    let error = err;
+
+   if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+   sendErrorProd(error, res);
   }
 
 };
