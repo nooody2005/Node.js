4c30ee9dcd7809541940a5ea92ac9fb4a1845935
Nada Hashem <Nooody2005std@gmail.com>
2026-03-16 03:08:08 +0200

    Handling Errors during Development vs production

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,10 +1,46 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'fail';
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+}
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+const sendErrorProd = (err, res) => {
+    //Operational, trusted error:send message to client
+    if(err.isOperational){
+
+        res.status(err.statusCode).json({
+          status: err.status,
+          message: err.message
+        });
+
+        //Programming or other unknown error: don't leak error details
+    }else{
+        console.log('errrrrror',err);
+
+        //send generic message   500 ---> maybe bug :(
+        res.status(500).json({
+          status: 'error',
+          message: 'something wrong :('
+        });
+    }
+};
+
+
+
+
+module.exports = (err, req, res, next) => {
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if(process.env.NODE_ENV === 'development'){
+    sendErrorDev(err,res);
+  }else if (process.env.NODE_ENV === 'production'){
+    sendErrorProd(err,res);
+  }
+
 };
 
