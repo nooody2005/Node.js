4c77b8245a9792ade0c2bdb51c822541bb4de732
Nada Hashem <Nooody2005std@gmail.com>
2026-03-17 14:14:46 +0200

    signing up users and create token

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -1,12 +1,22 @@
 const User = require('./../models/usermodel');
 const catchAsync = require('./../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 
 
 exports.signup = catchAsync(async (req,res,next) => {
-    const newUser = await User.create(req.body);
+    // const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        passwordConfirm:req.body.passwordConfirm
+    });
+                //jwt.sign({payload},secret);
+    const token = jwt.sign({id: newUser._id},process.env.JWT_SECRET,{expiresIn:process.env.JWT_EXPIRES});
 
     res.status(201).json({
         status:'success',
+        token,
         data:{
             user:newUser,
         }
