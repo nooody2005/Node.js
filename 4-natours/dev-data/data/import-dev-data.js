bccfa49a5117c6d2fc892fb929f69381587959f5
Nada Hashem <Nooody2005std@gmail.com>
2026-04-24 20:17:10 +0300

    start modelling and designing our data model the modelling tour guides done :)

--- 4-natours/dev-data/data/import-dev-data.js
+++ 4-natours/dev-data/data/import-dev-data.js
@@ -1,22 +1,30 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+// dotenv.config();
 const Tour = require('./../../models/tourModel');
 const { exit } = require('process');
 dotenv.config({ path: './config.env' });
 
 // const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-// const DB = 'mongodb://127.0.0.1:27017/natours';
-const DB = process.env.DATABASE;
+const DB = 'mongodb://127.0.0.1:27017/natours';
+// const DB = process.env.DATABASE;
 
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-})
-.then(() => console.log('DB connection successful'))
-.catch(err => console.log('DB connection error:', err));
+// mongoose.connect(DB, {
+//   useNewUrlParser: true,
+// })
+// .then(() => console.log('DB connection successful'))
+// .catch(err => console.log('DB connection error:', err));
+
+// const DB = process.env.DATABASE;
+console.log(DB);
+mongoose
+  .connect(DB)
+  .then(() => console.log('DB connection successful'))
+  .catch(err => console.log('DB connection error:', err));
 
 //Read file
-const tours =JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+const tours =JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 
 const importData = async() => {
--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,99 +1,122 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
-
+const User = require('./usermodel');
 //make collection (table)
 
-const tourSchema = new mongoose.Schema({
-        name: {
-            type: String,
-            required: [true, 'A tour must have a name'],
-            unique: true,
-            trim: true,
-            maxlength: [40,'name must be less than 40 characters'],
-            minlength: [10,'name must be above 10 characters'],
-            // validate : [validator.isAlpha,'name should contains characters only :)']
-        },
-
-        duration: {
-            type: Number,
-            required: [true, 'A tour must have a duration']
-        },
-
-        maxGroupSize: {
-            type: Number,
-            required: [true, 'A tour must have a group size']
-        },
-
-        difficulty: {
-            type: String,
-            required: [true, 'A tour must have a difficulty'],
-            enum :{
-                values : ['easy','medium','difficult'],
-                message : "difficulty must be eaither easy or medium or difficult :)"
-            }
-        },
-
-        ratingsAverage: {
-            type: Number,
-            default: 4.5,
-            min:[1,'rating must be above 1.0'],
-            max:[5,'rating must be less than 5']
-        },
-
-        ratingsQuantity: {
-            type: Number,
-            default: 0
-        },
-
-        price: {
-            type: Number,
-            required: [true, 'A tour must have a price']
-        },
-
-        priceDiscount:{
-            type : Number,
-            validate: 
-            {
-                validator : function(val){
-                    //this only points to the new DOC in new document creation
-                    return val < this.price;
-                },
-                message: 'price dicount ({VALUE}) should be below the regular price'
-            }
-        } ,
-
-        summary: {
-            type: String,
-            trim: true,
-            required: [true, 'A tour must have a description']
-        },
-
-        imageCover: {
-            type: String,
-            required: [true, 'A tour must have a cover image']
-        },
-
-        images: [String],
-
-        createdAt: {
-            type: Date,
-            default: Date.now()
-        },
-
-        startDates: [Date] ,
-
-        secretTour: {
-            type : Boolean,
-            default : false
-        }
-
-        },
-    {
-        toJSON : { virtuals : true},
-        toObject : { virtuals : true }
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+      maxlength: [40, 'name must be less than 40 characters'],
+      minlength: [10, 'name must be above 10 characters']
+      // validate : [validator.isAlpha,'name should contains characters only :)']
     },
-    
+
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration']
+    },
+
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size']
+    },
+
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'difficulty must be eaither easy or medium or difficult :)'
+      }
+    },
+
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'rating must be above 1.0'],
+      max: [5, 'rating must be less than 5']
+    },
+
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price']
+    },
+
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function(val) {
+          //this only points to the new DOC in new document creation
+          return val < this.price;
+        },
+        message: 'price dicount ({VALUE}) should be below the regular price'
+      }
+    },
+
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description']
+    },
+
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image']
+    },
+
+    images: [String],
+
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+
+    startDates: [Date],
+
+    secretTour: {
+      type: Boolean,
+      default: false
+    },
+    startLocation: {
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ],
+    guides: Array
+  },
+
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
 );
 
 tourSchema.virtual('durationWeeks').get(function() {
@@ -104,6 +127,13 @@ tourSchema.virtual('durationWeeks').get(function() {
 
 tourSchema.pre('save', function(next) {
     this.slug = slugify(this.name, {lower: true});
+    next();
+});
+
+// only for new document        ..not update
+tourSchema.pre('save', async function(next){
+    const guidePromises = this.guides.map(async id => await User.findById(id));
+    this.guides = await Promise.all(guidePromises);
     next();
 });
 
