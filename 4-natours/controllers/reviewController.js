2e8ab690ca524a77fdbfd117ceabaac68b4f0554
Nada Hashem <Nooody2005std@gmail.com>
2026-05-04 18:56:20 +0300

    adding new route to get all reviews in a tour done :)

--- 4-natours/controllers/reviewController.js
+++ 4-natours/controllers/reviewController.js
@@ -3,7 +3,12 @@ const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-    const reviews = await Review.find();
+
+    let filter = {};    //to get all reviews on a tour
+    if(req.params.tourId)  filter = {tour : req.params.tourId};
+
+
+    const reviews = await Review.find(filter);
 
     res.status(200).json({
         status: 'success',
