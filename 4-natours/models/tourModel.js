1080886e8873a74b83a91bb8627391659bc425b5
Nada Hashem <Nooody2005std@gmail.com>
2026-05-07 00:27:21 +0300

    Preventing Duplicate reviews problem done ^_^

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -39,7 +39,8 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       default: 4.5,
       min: [1, 'rating must be above 1.0'],
-      max: [5, 'rating must be less than 5']
+      max: [5, 'rating must be less than 5'],
+      set: val => Math.round(val * 10 ) / 10      // to round the value of rating like this 4.666666 ---> 46.6666 ---> 47 ---> 4.7
     },
 
     ratingsQuantity: {
