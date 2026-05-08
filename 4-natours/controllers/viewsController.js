110d31483930fe69eb14feabea99343682b72b88
Nada Hashem <Nooody2005std@gmail.com>
2026-05-09 02:04:59 +0300

    start Building the Tour Overview

--- 4-natours/controllers/viewsController.js
+++ 4-natours/controllers/viewsController.js
@@ -1,9 +1,20 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync'); 
+
+exports.getOverview = catchAsync(async(req, res, next) => {
+  // 1) get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build Templates
+
+  //3) Render the template using data from 1)
+
   res.status(200).render('overview', {
-    title: 'All tours'
+    title: 'All tours',
+    tours
   });
 
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
