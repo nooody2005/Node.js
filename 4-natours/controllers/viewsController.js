0466f6def7c787522679811396473b7d7851be0e
Nada Hashem <Nooody2005std@gmail.com>
2026-05-10 02:39:36 +0300

    start including a Map

--- 4-natours/controllers/viewsController.js
+++ 4-natours/controllers/viewsController.js
@@ -4,7 +4,7 @@ const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async(req, res, next) => {
   // 1) get tour data from collection
-  
+
   // const tours = await Tour.find();
   const tours = await Tour.find().lean();
   // console.log(tours[0]);
@@ -37,7 +37,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
   // 2) Build template
   // 3) Render template using data from 1)
   res.status(200).render('tour', {
-    title: 'try',
+    title: `${tour.name} Tour`,
     tour
   });
 });
