57009a78279c7c43eaf65e50dc5d49d3f36336c7
Nada Hashem <Nooody2005std@gmail.com>
2026-06-26 02:35:35 +0300

    Integrating Stripe into the Back-End done :)

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRouter');
 const { title } = require('process');
 
@@ -87,6 +88,7 @@ app.use('/',viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews',reviewRouter);
+app.use('/api/v1/booking', bookingRouter);
 
 app.all('*', (req, res, next) => {
   //make object from class AppError in the file appError in utils folder  and send this error to it 
---
+++ 4-natours/controllers/bookingController.js
@@ -0,0 +1,63 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const handleFactory = require('./../controllers/handleFactory');
+
+exports.getCheckoutSession = catchAsync(async(req, res, next) => {
+    // 1) Get the currently booked tour
+    const tour = await Tour.findById(req.params.tourId);
+
+    // 2) Create Checkout session
+    // const session = await stripe.checkout.sessions.create({
+    //   payment_method_types: ['card'],
+    //   success_url: `${req.protocol}://${req.get('host')}/`,
+    //   cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    //   customer_email: req.user.email,
+    //   client_reference_id: req.params.tourId,
+    //   line_items: [
+    //     {
+    //       name: `${tour.name} Tour`,
+    //       description: tour.summary,
+    //       images: [`http://127.0.0.1:8000/img/tours/${tour.imageCover}`],
+    //       amount: tour.price * 100,
+    //       currency:'usd',
+    //       quantity: 1
+    //     }
+    //   ]
+    // });
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+
+      success_url: `${req.protocol}://${req.get('host')}/`,
+      cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+
+      customer_email: req.user.email,
+      client_reference_id: req.params.tourId,
+
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: `${tour.name} Tour`,
+              description: tour.summary,
+              images: [`http://127.0.0.1:8000/img/tours/${tour.imageCover}`]
+            },
+            unit_amount: tour.price * 100
+          },
+          quantity: 1
+        }
+      ]
+    });
+
+    // 3) Create session as response
+    res.status(200).json({
+        status: 'success',
+        session
+    });
+    
+});
