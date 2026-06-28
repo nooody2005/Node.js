985e35f6a0a73c6e85437229601f561a658a5b98
Nada Hashem <Nooody2005std@gmail.com>
2026-06-28 19:58:46 +0300

    Creating New Bookings on checkout Success ..

--- 4-natours/controllers/bookingController.js
+++ 4-natours/controllers/bookingController.js
@@ -1,38 +1,21 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const Tour = require('./../models/tourModel');
-const APIFeatures = require('./../utils/apiFeatures');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
-const handleFactory = require('./../controllers/handleFactory');
+const Tour = require('../models/tourModel');
+const Booking =require('../models/bookingModel');
+const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const handleFactory = require('../controllers/handleFactory');
 
 exports.getCheckoutSession = catchAsync(async(req, res, next) => {
     // 1) Get the currently booked tour
     const tour = await Tour.findById(req.params.tourId);
 
     // 2) Create Checkout session
-    // const session = await stripe.checkout.sessions.create({
-    //   payment_method_types: ['card'],
-    //   success_url: `${req.protocol}://${req.get('host')}/`,
-    //   cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
-    //   customer_email: req.user.email,
-    //   client_reference_id: req.params.tourId,
-    //   line_items: [
-    //     {
-    //       name: `${tour.name} Tour`,
-    //       description: tour.summary,
-    //       images: [`http://127.0.0.1:8000/img/tours/${tour.imageCover}`],
-    //       amount: tour.price * 100,
-    //       currency:'usd',
-    //       quantity: 1
-    //     }
-    //   ]
-    // });
-
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       mode: 'payment',
 
-      success_url: `${req.protocol}://${req.get('host')}/`,
+      success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
       cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
 
       customer_email: req.user.email,
@@ -60,4 +43,16 @@ exports.getCheckoutSession = catchAsync(async(req, res, next) => {
         session
     });
     
+});
+
+exports.createBookingCheckout = catchAsync(async(req,res,next) => {
+    // This is only temperory, because it's unsecure: everyone can make bookings without payings
+    const { tour , user , price }= req.query;
+
+    if( !tour && !user && !price )    return next();
+    
+    await Booking.create({tour, user , price});
+
+    res.redirect(req.originalUrl.split('?')[0]);
+
 });
